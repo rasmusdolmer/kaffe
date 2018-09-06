fe3fa755385d593dbf201cb0ed2613c4224de0bd
Rasmus Dolmer Andersen <rasmusdolmer@gmail.com>
2018-09-06 09:27:30 +0200

    Har tilføjet klassen FlatWhite, som indeholder endnu en property, styrke.

---
+++ kaffe/FlatWhite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+    class FlatWhite : Kaffe , Imælk
+    {
+        public override int Pris()
+        {
+            return 45;
+        }
+
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+    }
+}
+
